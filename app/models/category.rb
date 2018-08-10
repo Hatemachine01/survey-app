1b43dcf035d5bc78237ce727e0d749ad345dc9bb
javoor1 <javoor1@hotmail.com>
2018-08-10 14:13:56 -0500

    models category validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
-  has_many :questions
+  has_many :questions, dependent: :destroy
+  validates :category_name, :threshold1, :threshold2, :escala_bajo, :escala_medio, :escala_alto, presence: :true
 end
