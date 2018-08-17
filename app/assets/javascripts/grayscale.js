c033af080f19e0854335e747cc04ea7554e52511
Julio <juliodevasquez@gmail.com>
2018-08-16 23:57:21 -0400

    Optimized IMGS + Read More Functionatlies + if mobile functionalities

--- app/assets/javascripts/grayscale.js
+++ app/assets/javascripts/grayscale.js
@@ -1,9 +1,30 @@
+///JQUERY FOR READ MORE FUNCTIONALITIES
+
+$(document).ready(function() {
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      //Stuff to do when btn is in the read more state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      //Stuff to do when btn is in the read less state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
+  });
+});
+
+
+
+
+
 ////JQUERY TO HIDE NAVBAR UPON SCROLLING DOWN
 
 $( document ).ready(function() {
     $(function() {
     $(document).on("mousewheel", function() {
-      if($(document).scrollTop() < 100){
+      if($(document).scrollTop() < 10){
           $('.navbar').show();
       } else {
           $('.navbar').hide();
