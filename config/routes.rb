2d6469f9013367a62bebf775c4364fe305543d25
javoor1 <javoor1@hotmail.com>
2018-08-20 20:32:05 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    resources :users
-    resources :answers
-    resources :categories
-    resources :questions
-
-    root to: "users#index"
-  end
-
   devise_for :users
 	root to: 'static#home'
 end
