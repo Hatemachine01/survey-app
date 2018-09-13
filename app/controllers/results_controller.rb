1637e93d88a3a96fb5077995f258a010d8ede88d
javoor1 <javoor1@hotmail.com>
2018-09-12 20:12:04 -0500

    lógica de creación de answers

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,4 +3,10 @@ class ResultsController < ApplicationController
 	def results
 		@answers = current_user.answers
 	end
+
+	#necesito la suma de los yes/no_value por category_id
+	def answers_total_value
+		
+	end
+
 end
